61781a8b103439dffe00533f94d4cf18c67ff895
Björn Guth <b.guth@netcon-consulting.com>
2021-08-01 22:00:15 +0200

    Modify my code to actually work
    This is thanks to the help of u/Master7432 on reddit. Based on their
    comment, I'll have to look into spawning a seperate thread to read the
    user input, though.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use futures::future::try_join_all;
 use std::fmt;
+use std::sync::Arc;
+use tokio::io::AsyncBufReadExt;
+use tokio::sync::Notify;
 
 #[derive(Debug)]
 enum MyError {
+    IO(String),
     Other,
 }
 
@@ -11,29 +15,43 @@ impl std::error::Error for MyError {}
 impl fmt::Display for MyError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Self::IO(e) => write!(f, "IO Error: {}", e),
             Self::Other => write!(f, "Some other error occured!"),
         }
     }
 }
 
-async fn do_some_work(task_name: &str) -> Result<(), MyError> {
-    println!("This is task {}, doing some work", task_name);
-    let reader = tokio::io::BufReader::new(tokio::io::stdin());
-    let buffer = Vec::new();
+impl From<std::io::Error> for MyError {
+    fn from(error: std::io::Error) -> Self {
+        Self::IO(error.to_string())
+    }
+}
 
-    let fut = tokio_io::io::read_until(reader, b'\n', buffer).await;
-    println!("Input was: {:?}", buffer);
+async fn do_some_work(task_name: &str, notify: Arc<Notify>) -> Result<(), MyError> {
+    notify.notified().await;
+    println!("This is task {}, doing some work", task_name);
+    notify.notify_one();
+    let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
+    let mut buffer = Vec::new();
+
+    notify.notified().await;
+    println!("Input for task {}, please?", task_name);
+    reader.read_until(b'\n', &mut buffer).await?;
+    println!("Input was: {:?}", String::from_utf8(buffer));
+    notify.notify_one();
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), MyError> {
-    let results = try_join_all(vec![
-        do_some_work("Task 1"),
-        do_some_work("Task 2"),
-        do_some_work("Task 3"),
-    ])
-    .await;
+    let notify = Arc::new(Notify::new());
+    let future_vec = vec![
+        do_some_work("Task 1", notify.clone()),
+        do_some_work("Task 2", notify.clone()),
+        do_some_work("Task 3", notify.clone()),
+    ];
+    notify.notify_one();
+    let results = try_join_all(future_vec).await;
     results?;
 
     Ok(())
